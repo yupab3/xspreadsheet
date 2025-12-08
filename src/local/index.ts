66c518aa80e5f007b22056560ec5220e44b170f8
KDY <yupab3@naver.com>
2025-12-09 07:45:33 +0900

    upload

--- src/local/index.ts
+++ src/local/index.ts
@@ -157,8 +157,8 @@ export class LocalSpreadsheet {
         this.table.reRenderCell(r, c)
       }
     }
-    this.table.editor && this.table.editor.setStyle(this.ss.currentCell())
-    this.editorbar && this.editorbar.setValue(this.ss.currentCell())
+    // this.table.editor && this.table.editor.setStyle(this.ss.currentCell())
+    // this.editorbar && this.editorbar.setValue(this.ss.currentCell())
     this.refreshToolbar()
     // this.ss.change(this.ss.data)
     return
